9dc5757af131b0ff98297801c0a4e9538d0b5fd3
Alec Cunningham <aleccunningham96@gmail.com>
2024-10-28 19:54:13 -0500

    feat: add types, hooks (#5)
    Co-authored-by: Alec Cunningham <aleccunninghm96@gmail.com>

--- lib/constants.tsx
+++
@@ -1,3 +0,0 @@
-export const EXAMPLE_PATH = "cms-ghost";
-export const CMS_NAME = "Ghost";
-export const CMS_URL = "https://ghost.org/";
--- utils.ts
+++ utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const graphQLAPI = 'https://swr-graphql-books-api.trillionz.repl.co';
+export const restAPI = 'http://localhost:5000';
